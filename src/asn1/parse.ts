c57cef09e489014b8c2cb82e53d7a76804183ca3
Seokseong Jeon <sjeon87@gmail.com>
2019-03-24 18:41:56 +0900

    Add ans1 parser CLI

--- src/asn1/parse.ts
+++ src/asn1/parse.ts
@@ -1,10 +1,12 @@
-import * as antlr4 from 'antlr4';
+import { readFile } from 'fs';
 
+import * as antlr4 from 'antlr4';
 import { ASNLexer } from './ASNLexer';
 import { ASNListener } from './ASNListener';
 import { ASNParser } from './ASNParser';
 import { ModulesVisitor } from './visitor/modules';
 
+import { sanitize } from '../utils/text';
 
 export function parse(text: string): any /* TODO */ {
   const chars = new antlr4.InputStream(text);
@@ -14,4 +16,17 @@ export function parse(text: string): any /* TODO */ {
   parser.buildParseTrees = true;
   const tree = parser.modules();
   return tree.accept(new ModulesVisitor());
+}
+
+if (require.main === module) {
+  const filePath = process.argv[2];
+  if (!filePath) {
+    throw Error('Requires an argument, filePath');
+  }
+  readFile(filePath, 'utf8', (err: Error, text: string) => {
+    if (err) {
+      throw err;
+    }
+    process.stdout.write(JSON.stringify(parse(sanitize(text)), null, 2));
+  });
 }
